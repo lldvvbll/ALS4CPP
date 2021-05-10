9872981f29906ff2af9427d94378c74ede975080
류현우 <lldvvbll@nate.com>
2021-05-10 23:01:31 +0900

    BaseCharacter의 BeginPlay 함수 구현

--- Source/ALS4CPP/Private/CharacterLogic/ALS_PlayerController_CPP.cpp
+++ Source/ALS4CPP/Private/CharacterLogic/ALS_PlayerController_CPP.cpp
@@ -3,6 +3,26 @@
 
 #include "CharacterLogic/ALS_PlayerController_CPP.h"
 #include "CameraSystem/ALS_PlayerCameraManager_CPP.h"
+#include "CharacterLogic/ALS_BaseCharacter_CPP.h"
+#include "Blueprint/UserWidget.h"
+#include "EngineUtils.h"
+
+AALS_PlayerController_CPP::AALS_PlayerController_CPP()
+{
+	static ConstructorHelpers::FClassFinder<UUserWidget> ALS_HUD_CLASS(
+		TEXT("/Game/AdvancedLocomotionV4/Blueprints/UI/ALS_HUD.ALS_HUD_C"));
+	if (ALS_HUD_CLASS.Succeeded())
+	{
+		ALS_HudClass = ALS_HUD_CLASS.Class;
+	}
+
+	static ConstructorHelpers::FClassFinder<UUserWidget> OVERLAY_SWITCHER_CLASS(
+		TEXT("/Game/AdvancedLocomotionV4/Blueprints/UI/OverlayStateSwitcher.OverlayStateSwitcher_C"));
+	if (OVERLAY_SWITCHER_CLASS.Succeeded())
+	{
+		OverlaySwitcherClass = OVERLAY_SWITCHER_CLASS.Class;
+	}
+}
 
 void AALS_PlayerController_CPP::OnPossess(APawn* aPawn)
 {
@@ -13,4 +33,19 @@ void AALS_PlayerController_CPP::OnPossess(APawn* aPawn)
 	{
 		ALS_PlayerCameraManager->OnPossess(aPawn);
 	}
+}
+
+void AALS_PlayerController_CPP::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ALS_HUD = CreateWidget<UUserWidget>(this, ALS_HudClass);
+	ALS_HUD->AddToViewport();
+
+	for (const auto& Char : TActorRange<AALS_BaseCharacter_CPP>(GetWorld()))
+	{
+		AvailableDebugCharacters.Add(Char);
+	}
+
+	DebugFocusCharacter = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
 }
--- Source/ALS4CPP/Public/CharacterLogic/ALS_PlayerController_CPP.h
+++ Source/ALS4CPP/Public/CharacterLogic/ALS_PlayerController_CPP.h
@@ -7,12 +7,57 @@
 #include "Interface/ALS_ControllerInterface.h"
 #include "ALS_PlayerController_CPP.generated.h"
 
+class UUserWidget;
+
 UCLASS()
 class ALS4CPP_API AALS_PlayerController_CPP : public APlayerController, public IALS_ControllerInterface
 {
 	GENERATED_BODY()
 
+public:
+	AALS_PlayerController_CPP();
+
 protected:
 	virtual void OnPossess(APawn* aPawn) override;
+	virtual void BeginPlay() override;
 
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Debug, Meta = (AllowPrivateAccess = true))
+	ACharacter* DebugFocusCharacter;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Debug, Meta = (AllowPrivateAccess = true))
+	TArray<ACharacter*> AvailableDebugCharacters;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Debug, Meta = (AllowPrivateAccess = true))
+	bool bShowHUD;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Debug, Meta = (AllowPrivateAccess = true))
+	bool bDebugView;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Debug, Meta = (AllowPrivateAccess = true))
+	bool bShowTraces;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Debug, Meta = (AllowPrivateAccess = true))
+	bool bShowDebugShapes;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Debug, Meta = (AllowPrivateAccess = true))
+	bool bShowLayerColors;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Debug, Meta = (AllowPrivateAccess = true))
+	bool bShowCharacterInfo;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Debug, Meta = (AllowPrivateAccess = true))
+	bool bSlomo;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Meta = (AllowPrivateAccess = true))
+	TSubclassOf<UUserWidget> ALS_HudClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Meta = (AllowPrivateAccess = true))
+	UUserWidget* ALS_HUD;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Meta = (AllowPrivateAccess = true))
+	TSubclassOf<UUserWidget> OverlaySwitcherClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Meta = (AllowPrivateAccess = true))
+	UUserWidget* OverlaySwitcher;
 };
